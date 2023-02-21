d3755e5d3669bd354288b02f611bdbabb8ab3acd
ccErrors <maxvinza3@gmail.com>
2023-02-21 15:31:54 +0200

    add front

---
+++ ku-a-su-front/src/components/MainPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>Ku-a-su</div>
+    <div>
+      <div>
+        <div>Subotica</div>
+        <div>/</div>
+      </div>
+      <div>
+        <div>Center III</div>
+        <div>/</div>
+      </div>
+      <div>
+        <div>Jo Lajoša</div>
+        <div>/</div>
+      </div>
+      <div>
+        <div>2</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
